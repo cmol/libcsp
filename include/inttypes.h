f4cb46175e92b5f68bd96fa260441d2fed9d39d3
Claus Lensbøl <cmol@cmol.dk>
2015-06-04 11:25:48 +0200

    Add typedef for eCos. TODO: Needs to be moved and included on eCos only.

---
+++ include/inttypes.h
@@ -0,0 +1,17 @@
+#ifndef CYGONCE_INTTYPES_H
+#define CYGONCE_INTTYPES_H
+#include <cyg/infra/cyg_type.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PRIu16   "u"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // CYGONCE_STDINT_H
---
+++ include/stdint.h
@@ -0,0 +1,27 @@
+#ifndef CYGONCE_STDINT_H
+#define CYGONCE_STDINT_H
+#include <cyg/infra/cyg_type.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef cyg_int8 int8_t ;
+typedef cyg_uint8 uint8_t ;
+
+typedef cyg_int16 int16_t;
+typedef cyg_uint16 uint16_t;
+
+typedef cyg_int32 int32_t;
+typedef cyg_uint32 uint32_t;
+
+typedef cyg_int64 int64_t;
+typedef cyg_uint64 uint64_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // CYGONCE_STDINT_H
